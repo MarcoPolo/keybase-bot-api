5189e50e90badaf02359286209070915292813d5
Marco Munizaga <marco@marcopolo.io>
2019-08-28 09:09:37 -0400

    Use keybase-protocol file

--- examples/avdl_parse.rs
+++ examples/avdl_parse.rs
@@ -1,11 +1,11 @@
-use std::fs;
 use pest::Parser;
 use rusty_keybase_bot::avdl::{self, AVDLParser};
+use std::fs;
 
 // @namespace("keybase.1")
 fn main() {
-  let input = fs::read_to_string("common.avdl")
-        .expect("Something went wrong reading the file");
+  let input = fs::read_to_string("keybase-protocol/keybase1/common.avdl")
+    .expect("Something went wrong reading the file");
   let parsed = AVDLParser::parse(avdl::Rule::avdl_protocol, &input);
   if let Err(e) = parsed {
     println!("{}", e);
